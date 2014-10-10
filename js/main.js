1e2354eac5219d64502559bd7aab7473c7ed75f3
thirdchance57 <thirdchance57@hotmail.com>
2014-10-10 15:26:40 -0700

    O image added toggle switch working

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
-console.log("working") ;
+console.log("main.js linked") ; //js conneciton tester
 
 var app = angular.module('myApp', []);
 
 app.controller("myController", function ($scope) {
+  
   $scope.boxes = [
-    {status: "X"}, {status: "X"}, {status: "X"}, 
-    {status: "X"}, {status: "X"}, {status: "X"}, 
-    {status: "X"}, {status: "X"}, {status: "X"}, ] ;
-}) ;
+    {number: "0"}, 
+    {number: "1"}, 
+    {number: "2"}, 
+    {number: "3"}, 
+    {number: "4"}, 
+    {number: "5"}, 
+    {number: "6"}, 
+    {number: "7"}, 
+    {number: "8"}, 
+    ]; //end of boxes array
+
+  $scope.images = [
+    { "src": "../images/picX.png"},
+    { "src": "../images/picO.png"}
+    ]; //end of images array
+
+  $scope.toggle = true ;
+  $scope.toggleCustom = function() {
+    $scope.toggle = $scope.toggle === false ? true: false;
+    console.log($scope.toggle);
+  }; //end of toggle function
+
+  
+
+
+
+}) ; //end of controller
