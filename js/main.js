72a1d59bfe29cea178c53288aab567de2647fc65
thirdchance57 <thirdchance57@hotmail.com>
2014-10-10 17:25:34 -0700

    readme added toggle connected to click, logging boxes to players

--- js/main.js
+++ js/main.js
@@ -5,30 +5,54 @@ var app = angular.module('myApp', []);
 app.controller("myController", function ($scope) {
   
   $scope.boxes = [
-    {number: "0"}, 
-    {number: "1"}, 
-    {number: "2"}, 
-    {number: "3"}, 
-    {number: "4"}, 
-    {number: "5"}, 
-    {number: "6"}, 
-    {number: "7"}, 
-    {number: "8"}, 
+    {number: 0}, 
+    {number: 1}, 
+    {number: 2}, 
+    {number: 3}, 
+    {number: 4}, 
+    {number: 5}, 
+    {number: 6}, 
+    {number: 7}, 
+    {number: 8}, 
     ]; //end of boxes array
+  $scope.winningArrays = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]; // end of possible winning combos winningArrays
+
+  $scope.playerLog1 = []; // player 1 choices
+  $scope.playerLog2 = []; // player 2 choices
 
   $scope.images = [
     { "src": "../images/picX.png"},
     { "src": "../images/picO.png"}
     ]; //end of images array
 
-  $scope.toggle = true ;
+  $scope.toggle = false ;
   $scope.toggleCustom = function() {
     $scope.toggle = $scope.toggle === false ? true: false;
     console.log($scope.toggle);
-  }; //end of toggle function
+  }; //end of toggle 
 
-  
+  $scope.logClick = function(thisCell) {
+    if ($scope.toggle === true) {
+      $scope.playerLog1.push(thisCell.number);
+      console.log(thisCell.number);
+      console.log($scope.playerLog1);
+    }
+    else{
+      $scope.playerLog2.push(thisCell.number);
+      console.log(thisCell.number);
+      console.log($scope.playerLog2);
 
+    }
+  };
 
 
 }) ; //end of controller
---
+++ specs/readme.txt
@@ -0,0 +1,13 @@
+1st draft
+make board
+make cells clickable
+make click 1 x
+make click 2 0
+create winning numbers array
+toggle function
+
+2nd draft
+change toggle
+load image - check toggle then load ing
+log 1&2 - check toggle then log.push
+check winning num to log
